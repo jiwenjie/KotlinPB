1d2ee59ac58e5675a6041870a5296e906fe70d9a
kuky <kukyxingshi@163.com>
2018-11-19 15:43:17 +0800

    update BaseRecyclerAdapter: add position param for convertView function

--- app/src/main/java/com/kuky/base/kotlin/demo/vp/NewsFragment.kt
+++ app/src/main/java/com/kuky/base/kotlin/demo/vp/NewsFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.text.TextUtils
 import android.view.View
-import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.kuky.base.android.kotlin.RetrofitManager
